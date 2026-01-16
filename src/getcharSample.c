b02d6ffe6e7c962c7183fd604db95384c41fb94a
DongjuLee <dozoo0528@gmail.com>
2026-01-16 21:34:25 +0900

    문자 입출력 주석

--- src/getcharSample.c
+++ src/getcharSample.c
@@ -3,10 +3,10 @@
 //
 #include <stdio.h>
 int main(void) {
-    char ch = 0;
+    char ch = 0;// 변수 선언 +정의
 
-    ch = getchar();
-    putchar(ch);
+    ch = getchar(); //getchar '해석' 한 글자를 가지고 와라
+    putchar(ch); //putchar '해석' 값이 저장된 메모리 값을 읽거나 출력
     putchar('Z');
 
     return 0;
