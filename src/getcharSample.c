c39c620a41955e8c6172b3c3c135afef4eb976ee
DongjuLee <dozoo0528@gmail.com>
2026-01-16 21:11:06 +0900

    문자 입출력

---
+++ src/getcharSample.c
@@ -0,0 +1,13 @@
+//
+// Created by Dongju Lee on 2026. 1. 16..
+//
+#include <stdio.h>
+int main(void) {
+    char ch = 0;
+
+    ch = getchar();
+    putchar(ch);
+    putchar('Z');
+
+    return 0;
+}
