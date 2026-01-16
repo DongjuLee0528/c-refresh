c5f7ab5d02f571c161f106e5b82eed173e0c405d
DongjuLee <dozoo0528@gmail.com>
2026-01-16 21:48:43 +0900

    문자열 입출력

---
+++ src/chapter10/getSample.c
@@ -0,0 +1,16 @@
+//
+// Created by Dongju Lee on 2026. 1. 16..
+//
+#include <stdio.h>
+int main(void) {
+
+    char szName[32] ={0};
+
+    printf("이름을 입력하세요: ");
+    gets(szName); //gets()는 보안 결함 문제가 있음
+
+    printf("당신의 이름은 ");
+    puts(szName);
+    puts("입니다. ");
+    return 0;
+}
