ceb6fa982b25ce4031fe53e4333d09b67e88bf8e
DongjuLee <dozoo0528@gmail.com>
2026-05-04 23:26:02 +0900

    43장

---
+++ src/chapter11/modSample.c
@@ -0,0 +1,11 @@
+//
+// Created by Dongju Lee on 2026. 5. 4..
+//
+#include <stdio.h>
+int main(void) {
+    int x = 0;
+    scanf("%d", &x);
+
+    printf("몫 : %d\n", x / 3);
+    printf("나머지 : %d\n", x % 3);
+}
