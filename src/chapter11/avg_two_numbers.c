cac4d07c8226ab00049cd6eb391fae553d55f45d
DongjuLee <dozoo0528@gmail.com>
2026-05-05 16:37:41 +0900

    46장

--- src/chapter11/avg_two_numbers.c
+++ src/chapter11/avg_two_numbers.c
@@ -1,3 +1,18 @@
 //
 // Created by Dongju Lee on 2026. 5. 5..
 //
+#include <stdio.h>
+int main(void) {
+    int a =0;
+    int b =0;
+    int avg =0;
+
+    printf("두 수를 입력해주세요. EX) 2 5 :");
+    scanf("%d %d", &a ,&b);
+
+    avg = (a + b) / 2;
+
+    printf("두 수의 평균 값은 %d 입니다",avg);
+    return 0;
+
+}
