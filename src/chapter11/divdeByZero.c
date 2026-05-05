65e46049b243fe6ee56cb773ff8d7a871ae5cd54
DongjuLee <dozoo0528@gmail.com>
2026-05-05 16:33:21 +0900

    45장

---
+++ src/chapter11/divdeByZero.c
@@ -0,0 +1,13 @@
+//
+// Created by Dongju Lee on 2026. 5. 5..
+//
+#include <stdio.h>
+int main(void) {
+    int nInput = 0;
+    printf("정수를 입력하세요.:");
+
+    scanf("%d", &nInput);
+    printf("목: %d\n",10 / nInput);
+
+    return 0;
+}
